b522a2626b6b209c60838d3f62500ef500401458
manasi <manasibharati94@gmail.com>
2024-12-06 10:01:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Chatbot from './Chatbot';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <Chatbot></Chatbot>
         </a>
       </header>
     </div>
